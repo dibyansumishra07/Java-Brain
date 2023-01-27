68759fb131ace043b03d9e19c5c77bd3a72c5e2e
Dibyansu Mishra <dibyansu.mishra07@gmail.com>
2023-01-27 19:18:44 +0530

    Problem Solving Programs

---
+++ src/Random/Test.java
@@ -0,0 +1,10 @@
+package Random;
+
+public class Test {
+    public static void main(String ara[]) {
+        try {
+        } catch (NumberFormatException e) {
+            System.out.println("galat he bhai");
+        }
+    }
+}
